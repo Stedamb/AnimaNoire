b285e50c24dcd4927d627ad35edbb6149994ddf0
Stefano <stedamb@gmail.com>
2024-10-25 19:54:35 +0200

    small footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <div className="text-center container-xl py-8">
         <p>
-          created with  ❤️<br />by Stefano D'ambrosio
+          AnimaNoire Tattoo Atelier
         </p>
       </div>
     );

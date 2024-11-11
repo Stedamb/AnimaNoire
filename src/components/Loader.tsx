22d525635e191994b4be263f3aae221dab00ea31
Stefano <stedamb@gmail.com>
2024-11-11 23:09:29 +0100

    fixed loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,9 @@
 import Cookies from 'js-cookie';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const Loader = () => {
+  const loaderRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
     const hasSeenLoader = Cookies.get('hasSeenLoader');
 
@@ -11,28 +13,34 @@ const Loader = () => {
       Cookies.set('hasSeenLoader', 'true');
 
       const windowScrolltimer = setTimeout(() => {
-          document.body.classList.remove('overflow-hidden');
-        }, 2500);
-        
-        const hideLoaderTimer = setTimeout(() => {
-            document.querySelector(".Loader")?.classList.add("hidden");
+        document.body.classList.remove('overflow-hidden');
+      }, 2500);
+
+      const hideLoaderTimer = setTimeout(() => {
+        if (loaderRef.current) {
+          loaderRef.current.classList.add("hidden");
+        }
       }, 3000);
 
       return () => {
         clearTimeout(windowScrolltimer);
         clearTimeout(hideLoaderTimer);
       };
+    } else {
+      if (loaderRef.current) {
+        loaderRef.current.classList.add("hidden");
+      }
     }
   }, []);
 
   return (
-    <div className="Loader">
+    <div ref={loaderRef} className="Loader">
       <div className="Logo opacity-0 mix-blend-lighten absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-150 z-10">
-        <img 
+        <img
           src="/logo.svg"
           alt="Logo"
-          width="640" 
-          height="640" 
+          width="640"
+          height="640"
           className="w-full h-auto"
         />
       </div>
